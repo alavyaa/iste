a7be51d250cdf10e3f71ef5a5b325ec9d77b93d7
Alavya Bajpai <bajpaialavya@gmail.com>
2026-06-22 22:02:41 +0530

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -37,7 +37,7 @@ const Nav: React.FC = () => {
           <img
             src={isteLogo}
             alt="ISTE Logo"
-            className="w-10 h-10 object-contain pixelated"
+            className="w-10 h-10 object-contain"
           />
 
           <div className="leading-none">
